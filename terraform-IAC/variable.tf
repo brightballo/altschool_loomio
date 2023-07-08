e5a9d35839129f74ae838fec0227bb63e341adb7
Bright Odey <62260900+starburn-hub@users.noreply.github.com>
2023-07-08 23:32:23 +0100

    Update variable.tf

--- terraform-IAC/variable.tf
+++ terraform-IAC/variable.tf
@@ -1,41 +1,34 @@
-variable "vpc_azs" {
-  description = "Availability zones for VPC"
-  type        = list(any)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-}
-
-variable "security_group_name" {
-  description = "Name of the security group"
-  type        = string
-  default     = "eks-cluster-sg"
-}
-
-variable "region" {
+# Description: This file contains all the variables used in the terraform code####################
+variable "AWS_REGION" {
   description = "AWS region"
   type        = string
   default     = "us-east-1"
 }
 
-variable "cluster_name" {
-  description = "Cluster Name"
+variable "env" {
+  description = "Environment variable abbreviation"
   type        = string
-  default     = "my-eks-cluster"
-}
+  default     = "prod"
 
-
-
-# Define the associations variable
-variable "associations" {
-  type = map(object({
-    action = string
-  }))
-
-  default = {
-    "alb-listener" = {
-      action = "ALLOW"
-    }
+  validation { 
+    condition     = contains(["staging", "prod"], var.env) #
+    error_message = "Invalid argument \"env\", please choose one of: (\"staging\",\"prod\")."
   }
 }
 
+### VPC variables
+variable "single_nat_gateway" {
+  type        = bool
+  default     = true
+  description = "Should be true if you want to provision a single shared NAT Gateway across all of your private networks. Consider setting to false for HA in production env."
+}
 
 
+### EKS variables
+variable "eks_default_instance_type" {
+  type        = string
+  default     = "t2.medium" # t2.medium is the default instance type for EKS
+  description = "Default instance type for EKS cluster"
+}
+
+############################################################################################################
