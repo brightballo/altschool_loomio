1ed4353058bb75d40635bd0e9b064424270a64e1
Bright Odey <62260900+starburn-hub@users.noreply.github.com>
2023-07-08 23:30:30 +0100

    Update output.tf

--- terraform-IAC/output.tf
+++ terraform-IAC/output.tf
@@ -1,26 +1,13 @@
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
-}
-
-output "cluster_security_group_id" {
-  description = "Security group ids attached to the cluster control plane"
-  value       = module.eks.cluster_security_group_id
-}
-
+# Description: Outputs for the EKS cluster####################
 output "region" {
   description = "AWS region"
-  value       = var.region
+  value       = var.AWS_REGION
 }
 
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
-}
-
-output "cluster_id" {
-  description = "Kubernetes Cluster ID"
-  value       = module.eks.cluster_id
+# EKS outputs
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane."
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_certificate_authority_data" {
@@ -28,7 +15,39 @@ output "cluster_certificate_authority_data" {
   sensitive = true
 }
 
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = module.eks.cluster_name
+}
 
 output "aws_auth_configmap_yaml" {
   value = module.eks.aws_auth_configmap_yaml
 }
+
+
+# VPC outputs
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "database_subnets" {
+  value = module.vpc.database_subnets
+}
+
+output "elasticache_subnets" {
+  value = module.vpc.elasticache_subnets
+}
+
+output "vpc_cidr_block" {
+  value = module.vpc.vpc_cidr_block
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+############################################################################################################
